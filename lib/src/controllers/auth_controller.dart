878f6896a1ff4dab9ebff77acc53dde209f93147
Mexl Delver Tuba <90206212+MexlTuba@users.noreply.github.com>
2024-06-28 14:56:35 +0800

    init, login working

--- lib/src/controllers/auth_controller.dart
+++ lib/src/controllers/auth_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:state_change_demo/src/enum/enum.dart';
@@ -14,7 +16,24 @@ class AuthController with ChangeNotifier {
   static AuthController get I => GetIt.instance<AuthController>();
 
   AuthState state = AuthState.unauthenticated;
+  late StreamSubscription<User?> currentAuthedUser;
+
   SimulatedAPI api = SimulatedAPI();
+  listen() {
+    currentAuthedUser =
+        FirebaseAuth.instance.authStateChanges().listen(handleUserChanges);
+  }
+
+  void handleUserChanges(User? user) {
+    print(user?.email);
+    print(user?.displayName);
+    if (user == null) {
+      state = AuthState.unauthenticated;
+    } else {
+      state = AuthState.authenticated;
+    }
+    notifyListeners();
+  }
 
   login(String userName, String password) async {
     bool isLoggedIn = await api.login(userName, password);
@@ -24,17 +43,23 @@ class AuthController with ChangeNotifier {
 
       notifyListeners();
     }
+    UserCredential userCredential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: userName, password: password);
   }
 
   ///write code to log out the user and add it to the home page.
   logout() {
     //should clear session
+    return FirebaseAuth.instance.signOut();
   }
 
   ///must be called in main before runApp
   ///
   loadSession() async {
     //check secure storage method
+    listen();
+    User? user = FirebaseAuth.instance.currentUser;
+    handleUserChanges(user);
   }
 
   ///https://pub.dev/packages/flutter_secure_storage or any caching dependency of your choice like localstorage, hive, or a db
